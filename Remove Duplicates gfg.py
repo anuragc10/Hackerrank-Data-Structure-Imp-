7b470146f399fe24d3a7392e983fe8eeadf844e9
anuragc10 <63997049+anuragc10@users.noreply.github.com>
2020-12-30 10:26:37 +0530

    Create Remove Duplicates gfg.py

---
+++ Remove Duplicates gfg.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+class Solution:
+	def removeDups(self, S):
+		l=list(s)
+		res = [] 
+        [res.append(x) for x in l if x not in res]
+		return "".join(res);
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		s = input()
+		
+		ob = Solution()	
+		answer = ob.removeDups(s)
+		
+		print(answer)
+
+
+# } Driver Code Ends
